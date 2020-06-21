beeb019b10d4fb94d07513dc7f9a2e57a7bdd28c
SherjeelAhmed <57128059+SherjeelAhmed@users.noreply.github.com>
2020-06-22 04:49:36 +0500

    changes in footer.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -5,17 +5,17 @@ import { GlobalContext } from "../context/GlobalState";
 export const AddTransaction = () => {
   
     const [text,setText]= useState("");
-    const [amount,setAmount]=useState(0);
+    const [amount,setAmount]=useState("");
 
     const { addTransaction } = useContext(GlobalContext);
 
     const onSubmit = e => {
       e.preventDefault();
-
+     
       const newTransaction = {
         id: Math.floor(Math.random() * 100000000),
         text,
-        amount: +amount
+        amount: +amount,
       }
 
       addTransaction(newTransaction);
@@ -33,8 +33,7 @@ export const AddTransaction = () => {
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
-            (negative - expense, positive - income)</label
-          >
+          </label>
           <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button  className="btn">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
     return (
         <div className = "footer">
-            Copyright &copy; Sherjeel Ahmed
+            Created by Sherjeel Ahmed
         </div>
     )
 }
